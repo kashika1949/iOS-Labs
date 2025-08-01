ad870677f0baf3ff944a28305f32f697d9f73e02
kashika1949 <kashika1949.be23@chitkara.edu.in>
2025-08-01 13:39:47 +0530

    Add files via upload
    upload

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Kashika Baweja on 29/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+@IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
+
