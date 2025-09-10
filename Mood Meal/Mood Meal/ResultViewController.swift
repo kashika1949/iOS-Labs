d59d68d45b051a63c9691e589e6c587e6f82accd
kashika1949 <kashika1949.be23@chitkara.edu.in>
2025-09-10 10:11:32 +0530

    Add files via upload
    upload

---
+++ Mood Meal/Mood Meal/ResultViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SummaryViewController.swift
+//  Mood Meal
+//
+//  Created by Kashika Baweja on 07/08/25.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+
+    var mood: Mood?
+    var activity: Activity?
+    
+    init?(coder: NSCoder, mood: Mood, activity: Activity) {
+        self.mood = mood
+        self.activity = activity
+        super.init(coder: coder)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Suggested Meal"
+        
+        if let mood, let activity {
+            updateUI(mood: mood, activity: activity)
+        }
+    }
+    
+    func updateUI(mood: Mood, activity: Activity) {
+        for meal in meals {
+            if meal.mood.name == mood.name && meal.activity.name == activity.name {
+                resultLabel.text = meal.suggestedMeal
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
